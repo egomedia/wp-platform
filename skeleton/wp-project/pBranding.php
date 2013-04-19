3fd546ba6c1ad40b76197c82775545ea826f061e
egomedia <oli@egomedia.co.uk>
2013-04-19 10:51:58 +0200

    Create pBranding.php

---
+++ skeleton/wp-project/pBranding.php
@@ -0,0 +1,14 @@
+<?php
+class pBranding extends oBranding {
+		
+	protected $name = 'Ego Media';
+	protected $url = 'http://www.egomedia.co.uk';
+	protected $login_logo = 'images/branding-login.png';
+	protected $admin_logo = 'images/branding-admin.png';
+	
+	function __construct() {
+		parent::__construct();
+	}
+		
+}
+?>
